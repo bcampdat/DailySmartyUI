71c4d5a63f91eee9c959b462e3a76b0b69668019
Bea <bcampdat@gmail.com>
2024-07-12 20:38:27 +0200

    logo component

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import Logo from "./logo";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <div>logo</div>
+      <div className="app">
+        {/* <h1>DevCamp React Starter</h1> */}
+        <Logo />
       </div>
     );
   }
