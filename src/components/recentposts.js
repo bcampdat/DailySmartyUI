341b6ac319197de28a984846c1a2b1e9e40b2160
Bea <bcampdat@gmail.com>
2024-07-16 19:09:12 +0200

    maping recentpost

--- src/components/recentposts.js
+++ src/components/recentposts.js
@@ -8,15 +8,30 @@ class RecentPosts extends Component {
   componentDidMount() {
     this.props.fetchRecentPosts();
   }
+
+  renderPosts = function() {
+    const posts = this.props.recentPosts.map((results, index) => {
+        if(index < 3) {
+            return (
+                <li key={index}>
+                    {results.name}
+                    {/* {post.title}    con la api dealy smarty */}
+                </li>
+            )   
+        }
+    })
+    return posts
+}
   render() {
     return (
       <div className="recent-posts">
         <div className="recent-posts__wrapper">
           <div className="recent-posts__heading">Recent Posts</div>
           <ul className="recent-posts__posts">
-            <li>recent post 0</li>
+            {/* <li>recent post 0</li>
             <li>recent post 1</li>
-            <li>recent post 2</li>
+            <li>recent post 2</li> */}
+            {this.renderPosts()}
           </ul>
         </div>
       </div>
@@ -24,4 +39,11 @@ class RecentPosts extends Component {
   }
 }
 
-export default connect(null, actions)(RecentPosts);
+// export default connect(null, actions)(RecentPosts);
+function mapStateToProps(state) {
+  return {
+      recentPosts: state.posts.recentPosts
+  }
+}
+
+export default connect(mapStateToProps, actions)(RecentPosts);
