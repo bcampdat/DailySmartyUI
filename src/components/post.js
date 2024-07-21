eeca4fa209d4b25d79f26bd6d1b707711b0e37c3
Bea <bcampdat@gmail.com>
2024-07-21 18:56:26 +0200

    React Animate Height

--- src/components/post.js
+++ src/components/post.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
+import AnimateHeight from "react-animate-height";
 
 class Post extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      height: 0,
+    };
+  }
   renderTopics() {
     let topics = this.props.vehicles.map((topic, index) => {
       return (
@@ -27,47 +35,50 @@ class Post extends Component {
   // }
   renderLinks() {
     let links = this.props.vehicles.map((results, index) => {
-        return (
-            <div className="post-link" key={index}>
-                <div className="post-link__box"></div>
-                <div className="post-link__link">
-                    <a href={results.url}>Useful Link #{index + 1}</a>
-                </div>
-            </div>
-        )
-    })
+      return (
+        <div className="post-link" key={index}>
+          <div className="post-link__box"></div>
+          <div className="post-link__link">
+            <a href={results.url}>Useful Link #{index + 1}</a>
+          </div>
+        </div>
+      );
+    });
     return links;
-}
-
-render() {
-  if(this.props.type == 'recent') {
-      return (
-          <li className="recent-post">
-              <div className="recent-post__title">
-                  {this.props.name}
-              </div>
-              <div className="recent-post__topics">
-                  {this.renderTopics()}
-              </div>
-          </li>
-      )
-  } else if(this.props.type == 'result') {
-      return (
-          <li className="result-post">
-              <div className="result-post__topics">
-                  {this.renderTopics()}
-              </div>
-              <div className="result-post__title">
-                  {this.props.name}
-              </div>
-              <div className="result-post__links">
-                  {this.renderLinks()}
-              </div>
-          </li>
-      )
   }
 
-}
+  render() {
+    if (this.props.type == "recent") {
+      return (
+        <li className="recent-post">
+          <div className="recent-post__title">{this.props.name}</div>
+          <div className="recent-post__topics">{this.renderTopics()}</div>
+        </li>
+      );
+    } else if (this.props.type == "result") {
+      return (
+        <li className="result-post">
+          <div className="result-post__topics">{this.renderTopics()}</div>
+          <div className="result-post__title">
+            {/* {this.props.name}
+              </div>
+              <div className="result-post__links">
+                  {this.renderLinks()} */}
+            <a
+              href={this.props.url_for_post}
+              onMouseEnter={() => this.setState({ height: 70 })}
+              onMouseLeave={() => this.setState({ height: 0 })}
+            >
+              {this.props.title}
+            </a>
+          </div>
+          <AnimateHeight duration={500} height={this.state.height}>
+            <div className="result-post__links">{this.renderLinks()}</div>
+          </AnimateHeight>
+        </li>
+      );
+    }
+  }
 }
 
 export default Post;
